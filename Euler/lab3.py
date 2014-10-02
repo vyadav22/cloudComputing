80d62b0df2b82ec48a5371e02244dd3266bc4da4
vyadav22 <vikas.yadav@student.dit.ie>
2014-10-02 17:46:28 +0000

    created file

--- Euler/lab3.py
+++ Euler/lab3.py
@@ -1,10 +1,10 @@
 total = 0
 
-//loop from 1 to 1000
+#loop from 1 to 1000
 for i in range(1,1000):
-  //if remender is 0 after dividing by 3 or 5
+  #if remender is 0 after dividing by 3 or 5
   if i %3 or i%5:
-    //i is added to total
+    #i is added to total
     total= i + total
 
 print(total)
