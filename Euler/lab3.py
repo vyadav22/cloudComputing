db08b29ef7f14df5476ce69e9100feb975603ae2
vyadav22 <vikas.yadav@student.dit.ie>
2014-10-02 21:41:36 +0100

    updated Q1

--- Euler/lab3.py
+++ Euler/lab3.py
@@ -3,7 +3,7 @@ total = 0
 #loop from 1 to 1000
 for i in range(1,1000):
   #if remender is 0 after dividing by 3 or 5
-  if i %3 or i%5:
+  if i %3 == 0 or i%5 == 0:
     #i is added to total
     total= i + total
 
